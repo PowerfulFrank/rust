e654b1305af0e30dfcb669ecf9cb0b99a5853c6d
franklinwillemen <franklinwillemen@protonmail.com>
2020-05-22 01:58:54 +0200

    finished raindrops

--- exercises/raindrops/src/lib.rs
+++ exercises/raindrops/src/lib.rs
@@ -1,3 +1,17 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+
+    let mut s = "".to_string();
+    let values = [(3, "Pling"),(5, "Plang"),(7, "Plong")];
+
+    for (i, sound) in values.iter() {
+        if n % i == 0 {
+            s.push_str(sound);
+        }
+    }
+
+    if s.is_empty(){
+        s = n.to_string();
+    }
+
+    s
 }
