681d4b705a4b0d2250b74c94b018ca0e969051cf
franklinwillemen <franklinwillemen@protonmail.com>
2020-05-22 04:14:28 +0200

    Bad implementation nth-prime

--- exercises/nth-prime/src/lib.rs
+++ exercises/nth-prime/src/lib.rs
@@ -1,3 +1,30 @@
 pub fn nth(n: u32) -> u32 {
-    unimplemented!("What is the 0-indexed {}th prime number?", n)
+
+    let mut counter:u32 = 0;
+    let mut p = 0;
+    let t = (n * 20)+10;
+
+    for i in 2..t {
+        if prime(i) {
+            if counter == n {
+                p = i;
+                break;
+            }
+            counter += 1;
+        }
+    }
+    p
+}
+
+pub fn prime(n: u32) -> bool {
+    if n <= 1 {
+        return false;
+    }
+
+    for a in 2..n {
+        if n % a == 0 {
+            return false; // if it is not the last statement you need to use `return`
+        }
+    }
+    true
 }
