f87ec112aea6b5a502e16b6b469a8df35484a29d
franklinwillemen <franklinwillemen@protonmail.com>
2020-05-22 23:20:17 +0200

    finished proverbs

--- exercises/proverb/src/lib.rs
+++ exercises/proverb/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+
+    let mut together = vec![];
+    let n = list.len();
+
+    if n == 0 {
+        String::new()
+    } else {
+        for i in 0..n-1 {
+            together.push(format!("For want of a {} the {} was lost.",list[i], list[i+1]));
+        }
+
+        together.push(format!("And all for the want of a {}.",list[0]));
+
+        together.join("\n")
+    }
+
 }
