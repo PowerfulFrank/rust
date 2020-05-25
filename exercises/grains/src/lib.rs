3af5a4913e415813d27bedec57d40908e6ced26d
PowerfulFrank <franklinwillemen@protonmail.com>
2020-05-25 12:07:17 +0200

    finished effecient implementation of grains

--- exercises/grains/src/lib.rs
+++ exercises/grains/src/lib.rs
@@ -1,7 +1,14 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!("grains of rice on square {}", s);
+    if s <= 0 || s > 64 {
+        panic!("Square must be between 1 and 64");
+    } else {
+        let b:u64 = 2;
+        b.pow(s-1)
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    let b:u64 = 2;
+    let board: Vec<u64> = (0..=63).map(|x| b.pow(x)).collect();
+    board.iter().sum()
 }
